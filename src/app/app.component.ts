571fe452fcd1e9b6528676a7fe4fe92bfc7c52ba
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-05-04 17:48:12 -0600

    learned how to use the @Output decorator, how to bind custom events and pass data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,23 @@ export class AppComponent {
     { type: "server", name: "Testserver", content: "Just a test" },
     { type: "blueprint", name: "Citlalli test", content: "Just another test" },
   ];
+
+  onServerAdded(severData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: "server",
+      name: severData.serverName,
+      content: severData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: "blueprint",
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
 }
